cb20315677069b890394e84cd6600803745fd984
yena <yena4150@swu.ac.kr>
2024-07-25 17:56:13 +0900

    manifest 오류 수정 및 MainActivity 파일명 변경

--- app/src/main/java/com/example/hackathon_guru/MainActivity.kt
+++ app/src/main/java/com/example/hackathon_guru/MainActivity.kt
@@ -3,9 +3,9 @@ package com.example.hackathon_guru
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
     }
 }
