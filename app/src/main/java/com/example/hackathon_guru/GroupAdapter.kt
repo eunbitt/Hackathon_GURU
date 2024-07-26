3e6c4de770f2db5d8f68c763d5c4ba726fe96fa2
eunbitt <eunbit2001@naver.com>
2024-07-26 13:22:58 +0900

    현재까지 작업 내용 임시저장용

---
+++ app/src/main/java/com/example/hackathon_guru/GroupAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.hackathon_guru
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GroupAdapter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_adapter)
+    }
+}
---
+++ app/src/main/java/com/example/hackathon_guru/GroupListMain.kt
@@ -0,0 +1,11 @@
+package com.example.hackathon_guru
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GroupListMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_list_main)
+    }
+}
