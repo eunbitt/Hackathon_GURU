682077aee4281ca30f1129ca8ce6dbeab2dc4ecc
eunbitt <eunbit2001@naver.com>
2024-07-26 13:50:12 +0900

    Resolve merge conflicts and complete merge with master

--- app/src/main/java/com/example/hackathon_guru/MainActivity.kt
+++ app/src/main/java/com/example/hackathon_guru/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.hackathon_guru
 import android.content.Intent
 import android.os.Bundle
 import com.kakao.sdk.common.KakaoSdk;
->>>>>>> master
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +12,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         KakaoSdk.init(this, getString(R.string.kakao_app_key));
->>>>>>> master
     }
 }
