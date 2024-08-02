5c67337afaf4d81d5e5e1a9418c11d47541b8286
EESEO <enrui2001@naver.com>
2024-08-02 15:17:15 +0900

    db 연결 전 버전

--- app/src/main/java/com/example/hackathon_guru/MyScrapFolderMoreButton.kt
+++ app/src/main/java/com/example/hackathon_guru/MyScrapFolderMoreButton.kt
@@ -6,13 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class item_scrap_folder : AppCompatActivity() {
+class MyScrapFolderMoreButton : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.item_scrap_folder)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.item_scrap_folder)) { v, insets ->
+        setContentView(R.layout.activity_my_scrap_folder_more_button)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
